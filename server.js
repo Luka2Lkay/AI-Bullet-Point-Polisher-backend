2e9f4218ee08cbb148f5569a99e95102ddc39f02
Luka <luka.matshebelele@gmail.com>
2026-03-23 11:11:28 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const OpenAI = require('openai');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
